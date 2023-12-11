f1b79c9f50e3cc55a2947c2fda07cb14ced1ff80
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-12-11 21:06:02 +0300

    VUE

---
+++ app/src/App.vue
@@ -0,0 +1,20 @@
+<!-- Тег JS вся JS печатается в теге <script> Атрибут setup обязателен при помощи него подключаем компоненты и подключение реактивных  -->
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<!-- Тег HTML весь код печатается в теге HTML <template> -->
+<template>
+  <div class="card__title">Загаловок</div>
+  <svg>
+    <use href></use>
+  </svg>
+  <Card title="рубашка 1" price="300000" />
+</template>
+
+<!-- Тег CSS весь код пишется в теге <style> -->
+<style>
+body {
+  background-color: rgb(107, 0, 0);
+}
+</style>
---
+++ app/src/components/Card.vue
@@ -0,0 +1,43 @@
+<!-- Тег JS вся JS печатается в теге <script> -->
+<script setup>
+// export default {
+//   data() {
+//     return {
+//       count: 0
+//     }
+//   },
+//   methods: {
+//     incriment() {
+//       this.count++
+//     }
+//   }
+// }
+
+// Композитный подход
+import { ref } from 'vue'
+let count = ref(0)
+function incriment() {
+  count.value++
+}
+</script>
+
+<!-- Тег HTML весь код печатается в теге HTML <template> -->
+<template>
+  <div class="card">
+    <div class="card__tittle">T-shot BOSS</div>
+    <div class="card__price">15.000 rub</div>
+  </div>
+  <!-- <h2>{{ count }}</h2>
+  <button @click="incriment">Добавить 1</button>
+  <input v-model="count" /> -->
+</template>
+
+<!-- Тег CSS весь код пишется в теге <style> -->
+<style scoped>
+body {
+  background-color: rgb(0, 104, 0);
+}
+.card__tittle {
+  font-size: 100px;
+}
+</style>
